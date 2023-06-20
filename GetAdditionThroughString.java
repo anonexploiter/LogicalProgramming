674035deab9af45d5ffe621157420378af4bdf64
Surendar <71928263+anonexploiter@users.noreply.github.com>
2023-06-20 23:33:04 +0530

    Update GetAdditionThroughString.java

--- GetAdditionThroughString.java
+++ GetAdditionThroughString.java
@@ -1,7 +1,7 @@
 public class GetAdditionThroughString {
      public static void main(String[] args) {
         String input1 = "The Weather is good";
-        String[] word = input1.split("\\s");
+        String[] word = input1.split("\\s"); //split the string by whitespaces
         int sum =0;
         for(int i = 0; i < word.length; i++) {
             sum += word[i].length(); //adding the splitted string
@@ -9,7 +9,7 @@ public class GetAdditionThroughString {
         
         int temp = 0;
         while(sum>0){
-            temp += sum%10; //splitting the string
+            temp += sum%10; //splitting the integer by last digit
             sum/=10;
         }        
 
